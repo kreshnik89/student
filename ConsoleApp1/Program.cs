e0bff0d99a85928d08489a3f3dcdc23d3c0c4878
kreshnik89 <kreshnik.raca@hotmail.com>
2017-04-18 19:34:13 +0200

    del1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detta är ett test och jag ska jobba med detta när jag är på jobbet");
+            Console.ReadKey();
+        }
+    }
+}
